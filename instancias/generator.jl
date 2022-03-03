17a5ebc1c0bfb2d353ba54406a1b4759f6b057cf
Eduardo Salazar <eduardosalaz@outlook.com>
2022-03-03 15:23:44 -0600

    Workaround por mientras, luego lo hago automatico

--- instancias/generator.jl
+++ instancias/generator.jl
@@ -5,7 +5,7 @@ function generate()
     S_coords = rand(10:500,(8,2))
     plot(scatter(BU_coords[:,1], BU_coords[:,2], lab="BU", marker = ([:hex :d], 5, 0.8, Plots.stroke(3, :gray))))
     ploteado = scatter!(S_coords[:,1], S_coords[:,2], lab="S", c=:black, markersize=:6)
-    png("ploteado.png")
+    png("plot1.png")
     return BU_coords, S_coords
 end
 
@@ -18,8 +18,33 @@ function distanceM(B,S)
 	   mat[i,j] = distancia
 	end
     end
-    open("distance_mat.txt", "w") do io
-        writedlm(io, mat)
+    open("instancia.txt", "w") do io
+	write(io, "D\n")
+	writedlm(io, mat, ' ')
+	write(io, "S\n")
+	write(io, "1 3\n") # s1
+	write(io, "2 5\n") # s2
+	write(io, "4\n") # s3
+	write(io, "6 7\n") #s4
+	write(io, "8\n") # s5
+
+	write(io, "U\n")
+    write(io, "2\n") #u1
+    write(io, "2\n")
+    write(io, "1\n")
+    write(io, "2\n")
+    write(io, "1\n")
+
+    write(io, "L\n")
+    write(io, "1\n") # l1  rango de 1 ≤ 2 = 1 o 2
+    write(io, "1\n") # l2 rango de 1 ≤ 2 = 1 o 2
+    write(io, "1\n") # l3 rango de 1 ≤ 1 = 1
+    write(io, "1\n") # l4 rango de 1 ≤ 2 = 1 o 2
+    write(io, "0\n") # l5 rango de 0 ≤ 1 = 0 o 1
+
+    write(io, "P\n")
+    write(io, "6\n") # solo 6 centros
+
     end
     return mat
 end
