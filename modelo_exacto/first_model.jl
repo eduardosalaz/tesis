f9b88f1d2c4c4429f0efa942dfb3e0e58e6e5689
Eduardo Salazar <eduardosalaz@outlook.com>
2022-03-06 14:41:06 -0600

    TYPES TYPES TYPES

--- modelo_exacto/first_model.jl
+++ modelo_exacto/first_model.jl
@@ -30,7 +30,7 @@ function read_file(path::String)
 
     # as for now the k param is hardcoded to 5
     k = 5
-    Sk = []
+    Sk = [Set{Int64}(1) for _ in 1:k] # terrible workaround
     Sk_line = endl+2
     j = 1
     for i in Sk_line:length(content)
@@ -38,10 +38,10 @@ function read_file(path::String)
             endl = i
             break
         end
-        push!(Sk, [parse(Int, distance) for distance in split(content[i])])
+        Sk[j] = Set([parse(Int, elem) for elem in split(content[i])])
         j = j +1
     end
-    Uk = [] # gotta move this to a function so DRY
+    Uk = Array{Int64}(undef, k) # gotta move this to a function so DRY
     Uk_line = endl + 1
     j = 1
     for i in Uk_line:length(content)
@@ -49,10 +49,10 @@ function read_file(path::String)
             endl = i
             break
         end
-        push!(Uk, parse(Int, content[i]))
+        Uk[j] = parse(Int, content[i])
         j = j +1
     end
-    Lk = []
+    Lk = Array{Int64}(undef, k)
     Lk_line = endl + 1
     j = 1
     for i in Lk_line:length(content)
@@ -60,53 +60,67 @@ function read_file(path::String)
             endl = i
             break
         end
-        push!(Lk, parse(Int, content[i]))
+        Lk[j] = parse(Int, content[i])
         j = j +1
     end
     endl = endl + 1
     P = parse(Int, content[endl])
     endl = endl + 2
-    R = [parse(Int, distance) for distance in split(content[endl])]
+    R = [parse(Int, elem) for elem in split(content[endl])]
     endl = endl + 2
-    V = []
+    m = 3 # activities
+    V = [Array{Int64}(undef,5) for _ in 1:m]
     j = 1
     for i in endl:length(content)
         if content[i] == "μ"
             endl = i
             break
         end
-        push!(V, [parse(Int, distance) for distance in split(content[i])])
+        V[j] = [parse(Int, value) for value in split(content[i])]
         j = j +1
     end
     endl = endl + 1
-    μ = []
+    μ = [Array{Int64}(undef,5) for _ in 1:m]
     j = 1
     for i in endl:length(content)
         if content[i] == "T"
             endl = i
             break
         end
-        push!(μ, [parse(Int, distance) for distance in split(content[i])])
+        μ[j] = [parse(Int, value) for value in split(content[i])]
         j = j +1
     end
     endl = endl + 1
-    T = []
+    T = Array{Float64}(undef, m)
     j = 1
     for i in endl:length(content)
         if content[i] == "α"
             endl = i
             break
         end
-        push!(T, parse(Float32, content[i]))
+        T[j] = parse(Float32, content[i]) # consider truncing
         j = j +1
     end
     endl = endl + 1
-    α = [parse(Int, distance) for distance in split(content[endl])]
+    α = [parse(Int, thresh) for thresh in split(content[endl])]
     return distance_matrix, Sk, Uk, Lk, P, R, V, μ, T, α
     # falta agregar asserts para comprobar dimensiones
 end
 
-function model()
+function model(data)
+
+    D = data[1] # matrix float 32
+    Sk = data[2] # vector any
+    Uk = data[3] #
+    Lk = data[4]
+    P = data[5]
+    R = data[6]
+    V = data[7]
+    μ = data[8]
+    T = data[9]
+    α = data[10]
+
+
 end
 
 
@@ -114,9 +128,7 @@ function main()
     path = "../instancias/instancia.txt"
     #ARGS[1]
     data = read_file(path)
-    for cosa in data
-        @show cosa
-    end
+    model(data)
 end
 
 main()
