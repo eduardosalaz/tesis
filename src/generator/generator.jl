f87c03ad021ccce981a8215e2fb1df83f42be2c7
Eduardo Salazar <eduardosalaz@outlook.com>
2022-04-20 15:16:33 -0500

    limpiar redundancias y cambiar estructura

--- src/generator/generator.jl
+++ src/generator/generator.jl
@@ -1,64 +1,36 @@
 include("../types/types.jl")
 using Distances, JLD2, Plots, Random, .Types
 
-function write_jld2(size::String)
+function generate_instance(size::String, i::Int)
     K = 5
     M = 3
+    B, S, P = 0
     if size == "S"
         B = 25
         S = 10
         P = 8
-        BU_coords, S_coords = generate_coords(B, S)
-        dist_mat = generate_dist(BU_coords, S_coords, B, S)
-        parameters = generate_params(size, B, S)
-        instance = Instance(B, S, K, M, P, BU_coords, S_coords, dist_mat,parameters...)
-        dir_path = "instances/"
-        file_name = "inst_new_" * size * ".jld2"
-        full_path = dir_path * file_name
-        jldsave(full_path; instance)
     elseif size == "M"
         B = 80
         S = 40
         P = 25
-        BU_coords, S_coords = generate_coords(B, S)
-        dist_mat = generate_dist(BU_coords, S_coords, B, S)
-        parameters = generate_params(size, B, S)
-        instance = Instance(B, S, K, M, P, BU_coords, S_coords, dist_mat,parameters...)
-        dir_path = "instances/"
-        file_name = "inst_" * size * ".jld2"
-        full_path = dir_path * file_name
-        jldsave(full_path; instance)
     elseif size == "L"
         B = 150
         S = 80
         P = 50
-        BU_coords, S_coords = generate_coords(B, S)
-        dist_mat = generate_dist(BU_coords, S_coords, B, S)
-        parameters = generate_params(size, B, S)
-        instance = Instance(B, S, K, M, P, BU_coords, S_coords, dist_mat,parameters...)
-        dir_path = "instances/"
-        file_name = "inst_" * size * ".jld2"
-        full_path = dir_path * file_name
-        jldsave(full_path; instance)
     elseif size == "XL"
         B = 300
         S = 120
         P = 70
-        BU_coords, S_coords = generate_coords(B, S)
-        dist_mat = generate_dist(BU_coords, S_coords, B, S)
-        parameters = generate_params(size, B, S)
-        instance = Instance(B, S, K, M, P, BU_coords, S_coords, dist_mat,parameters...)
-        dir_path = "instances/"
-        file_name = "inst_" * size * ".jld2"
-        full_path = dir_path * file_name
-        jldsave(full_path; instance)
-    else
-        @error "Size not recognized"
-        exit(1)
     end
-
+    BU_coords, S_coords = generate_coords(B, S)
+    dist_mat = generate_dist(BU_coords, S_coords, B, S)
+    parameters = generate_params(B, S)
+    instance = Instance(B, S, K, M, P, BU_coords, S_coords, dist_mat, parameters...)
+    dir_path = "instances_"* size * "/"
+    file_name = "inst_" * string(i) * ".jld2"
+    full_path = dir_path * file_name
+    write_instance(instance, full_path)
 end
-
 
 function generate_coords(B, S)
     BU_coords = rand(10:800, (B, 2))
@@ -79,195 +51,61 @@ function generate_dist(BU_coords, S_coords, B, S)
     return trunc.(Int, mat)
 end
 
-function generate_params(size::String, B::Int64, S::Int64)
-    if size == "S"
-        Sk, Lk, Uk = generate_k(size, B, S)
-        #@info "Wrote k"
-        V, μ, T = generate_activities(size, B, S)
-        #@info "Wrote activities"
-        R, β = generate_risk(size, B, S)
-        #@info "Wrote risk"
-        return Sk, Lk, Uk, V, μ, T, R, β
-    elseif size == "M"
-        Sk, Lk, Uk = generate_k(size, B, S)
-        # @info "Wrote k"
-        V, μ, T = generate_activities(size, B, S)
-        # @info "Wrote activities"
-        R, β = generate_risk(size, B, S)
-        # @info "Wrote risk"
-        return Sk, Lk, Uk, V, μ, T, R, β
-    elseif size == "L"
-        Sk, Lk, Uk = generate_k(size, B, S)
-        # @info "Wrote k"
-        V, μ, T = generate_activities(size, B, S)
-        # @info "Wrote activities"
-        R, β = generate_risk(size, B, S)
-        # @info "Wrote risk"
-        return Sk, Lk, Uk, V, μ, T, R, β
-    elseif size == "XL"
-        Sk, Lk, Uk = generate_k(size, B, S)
-        # @info "Wrote k"
-        V, μ, T = generate_activities(size, B, S)
-        # @info "Wrote activities"
-        R, β = generate_risk(size, B, S)
-        # @info "Wrote risk"
-        return Sk, Lk, Uk, V, μ, T, R, β
-    end
+function generate_params(B::Int64, S::Int64)
+    Sk, Lk, Uk = generate_k(B, S)
+    #@info "Wrote k"
+    V, μ, T = generate_activities(B, S)
+    #@info "Wrote activities"
+    R, β = generate_risk(B, S)
+    #@info "Wrote risk"
+    return Sk, Lk, Uk, V, μ, T, R, β
 end
 
-function generate_k(size::String, B::Int64, S::Int64)
-    if size == "S"
-        K = 5
-        Ks = rand(1:K, S)
-        Sk = []
-        for i in 1:K
-            Si = findall(x->x==i, Ks)
-            push!(Sk, Si)
-        end
-        Sk = convert(Vector{Vector{Int64}}, Sk)
-        counts_k = [length(s) for s in Sk]
-        Lk_flt = [k - 0.4k for k in counts_k]
-        Uk_flt = [k + 0.4k for k in counts_k]
-        Lk = trunc.(Int, Lk_flt)
-        Uk = trunc.(Int, Uk_flt)
-        return Sk, Lk, Uk
-    elseif size == "M"
-        K = 5
-        Ks = rand(1:K, S)
-        Sk = []
-        for i in 1:K
-            Si = findall(x->x==i, Ks)
-            push!(Sk, Si)
-        end
-        Sk = convert(Vector{Vector{Int64}}, Sk)
-        counts_k = [length(s) for s in Sk]
-        Lk_flt = [k - 0.4k for k in counts_k]
-        Uk_flt = [k + 0.4k for k in counts_k]
-        Lk = trunc.(Int, Lk_flt)
-        Uk = trunc.(Int, Uk_flt)
-        return Sk, Lk, Uk
-    elseif size == "L"
-        K = 5
-        Ks = rand(1:K, S)
-        Sk = []
-        for i in 1:K
-            Si = findall(x->x==i, Ks)
-            push!(Sk, Si)
-        end
-        Sk = convert(Vector{Vector{Int64}}, Sk)
-        counts_k = [length(s) for s in Sk]
-        Lk_flt = [k - 0.4k for k in counts_k]
-        Uk_flt = [k + 0.4k for k in counts_k]
-        Lk = trunc.(Int, Lk_flt)
-        Uk = trunc.(Int, Uk_flt)
-        return Sk, Lk, Uk
-    elseif size == "XL"
-        K = 5
-        Ks = rand(1:K, S)
-        Sk = []
-        for i in 1:K
-            Si = findall(x->x==i, Ks)
-            push!(Sk, Si)
-        end
-        Sk = convert(Vector{Vector{Int64}}, Sk)
-        counts_k = [length(s) for s in Sk]
-        Lk_flt = [k - 0.4k for k in counts_k]
-        Uk_flt = [k + 0.4k for k in counts_k]
-        Lk = trunc.(Int, Lk_flt)
-        Uk = trunc.(Int, Uk_flt)
-        return Sk, Lk, Uk
+function generate_k(B::Int64, S::Int64)
+    K = 5
+    Ks = rand(1:K, S)
+    Sk = []
+    for i in 1:K
+        Si = findall(x->x==i, Ks)
+        push!(Sk, Si)
     end
+    Sk = convert(Vector{Vector{Int64}}, Sk)
+    counts_k = [length(s) for s in Sk]
+    Lk_flt = [k - 0.4k for k in counts_k]
+    Uk_flt = [k + 0.4k for k in counts_k]
+    Lk = trunc.(Int, Lk_flt)
+    Uk = trunc.(Int, Uk_flt)
+    return Sk, Lk, Uk
 end
 
-function generate_activities(size::String, B::Int64, S::Int64)
+function generate_activities(B::Int64, S::Int64)
     M = 3
-    if size == "S"
-        V = [zeros(Int64, B) for _ in 1:M]
-        for i in 1:length(V)
-            V[i] = rand(10:30, B)
-        end
-        μ = [zeros(Int64, S) for _ in 1:M]
-        for i in 1:length(μ)
-            sum_vals = sum(V[i])
-            μ[i] = fill(trunc(Int, sum_vals/S), S)
-        end
-        T = fill(0.3, M)
-        return V, μ, T
-    elseif size == "M"
-        V = [zeros(Int64, B) for _ in 1:M]
-        for i in 1:length(V)
-            V[i] = rand(10:30, B)
-        end
-        μ = [zeros(Int64, S) for _ in 1:M]
-        for i in 1:length(μ)
-            sum_vals = sum(V[i])
-            μ[i] = fill(trunc(Int, sum_vals/S), S)
-        end
-        T = fill(0.3, M)
-        return V, μ, T
-    elseif size == "L"
-        V = [zeros(Int64, B) for _ in 1:M]
-        for i in 1:length(V)
-            V[i] = rand(10:30, B)
-        end
-        μ = [zeros(Int64, S) for _ in 1:M]
-        for i in 1:length(μ)
-            sum_vals = sum(V[i])
-            μ[i] = fill(trunc(Int, sum_vals/S), S)
-        end
-        T = fill(0.3, M)
-        return V, μ, T
-    elseif size == "XL"
-        V = [zeros(Int64, B) for _ in 1:M]
-        for i in 1:length(V)
-            V[i] = rand(10:30, B)
-        end
-        μ = [zeros(Int64, S) for _ in 1:M]
-        for i in 1:length(μ)
-            sum_vals = sum(V[i])
-            μ[i] = fill(trunc(Int, sum_vals/S), S)
-        end
-        T = fill(0.3, M)
-        return V, μ, T
+    V = [zeros(Int64, B) for _ in 1:M]
+    for i in 1:length(V)
+        V[i] = rand(10:30, B)
     end
+    μ = [zeros(Int64, S) for _ in 1:M]
+    for i in 1:length(μ)
+        sum_vals = sum(V[i])
+        μ[i] = fill(trunc(Int, sum_vals/S), S)
+    end
+    T = fill(0.3, M)
+    return V, μ, T
 end
 
 function generate_risk(size::String, B::Int64, S::Int64)
-    if size == "S"
-        R = rand(2:10, B)
-        sum_R = sum(R)
-        lower = trunc(Int, trunc(sum_R/S) - 0.01sum_R)
-        upper = trunc(Int, trunc(sum_R/S) + 0.01sum_R)
-        β = rand(lower:upper, S)
-        return R, β
-    elseif size == "M"
-        R = rand(10:20, B)
-        sum_R = sum(R)
-        lower = trunc(Int, trunc(sum_R/S) - 0.01sum_R)
-        upper = trunc(Int, trunc(sum_R/S) + 0.01sum_R)
-        β = rand(lower:upper, S)
-        return R, β
-    elseif size == "L"
-        R = rand(15:25, B)
-        sum_R = sum(R)
-        lower = trunc(Int, trunc(sum_R/S) - 0.01sum_R)
-        upper = trunc(Int, trunc(sum_R/S) + 0.01sum_R)
-        β = rand(lower:upper, S)
-        return R, β
-    elseif size == "XL"
-        R = rand(10:20, B)
-        sum_R = sum(R)
-        lower = trunc(Int, trunc(sum_R/S) - 0.001sum_R)
-        upper = trunc(Int, trunc(sum_R/S) + 0.001sum_R)
-	    β = trunc(Int, sum_R/S)
-        return R, β
-    end
+    R = rand(2:15, B)
+    sum_R = sum(R)
+    lower = trunc(Int, trunc(sum_R/S) - 0.01sum_R)
+    upper = trunc(Int, trunc(sum_R/S) + 0.01sum_R)
+    β = rand(lower:upper, S)
+    return R, β
 end
 
 function main()
-    size = ARGS[1]
-    write_jld2(size)
+    size = ARGS[1], n = Int(ARGS[2])
+    for i in 1:n
+        generate_instance(size, i)
+    end
     @info "Done"
 end
-
-# main()
