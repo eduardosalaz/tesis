8dd5f3cc45ffe4dbbc0472f99798fe90e1d49ab7
Eduardo Salazar <eduardosalaz@outlook.com>
2022-04-20 20:04:34 -0500

    plotear soluciones

--- src/types/utils.jl
+++ src/types/utils.jl
@@ -1,6 +1,6 @@
 using JLD2
 
-function plot_instance(Instance::Instance)
+function plot_instance(Instance::Instance, path::String)
     Plots.scatter(
         Instance.BU_coords[:, 1],
         Instance.BU_coords[:, 2],
@@ -20,11 +20,103 @@ function plot_instance(Instance::Instance)
     )
     sizeB = Instance.B
     sizeS = Instance.S
-    png("out/plots/plot_instance_bu$size" * "_suc" * "$sizeS" * ".png")
-    #@info "Wrote plot and coords"
+    png(path)
+    @debug "Wrote plot and coords"
 end
 
-function plot_solution(Solution::Solution)
+function plot_solution(Solution::Solution, path::String)
+    Instance = Solution.Instance
+    BU_coords = Instance.BU_coords
+    S_coords = Instance.S_coords
+    Y = Solution.Y
+    X = Solution.X
+    S = Instance.S
+    B = Instance.B
+    is_used = [(Y[j] ? :red : :white) for j in 1:S]
+    Sk = Instance.Sk
+    S₁ = Sk[1]
+    S₁_coords = S_coords[S₁, :]
+    S₂ = Sk[2]
+    S₂_coords = S_coords[S₂, :]
+    S₃ = Sk[3]
+    S₃_coords = S_coords[S₃, :]
+    S₄ = Sk[4]
+    S₄_coords = S_coords[S₄, :]
+    S₅ = Sk[5]
+    S₅_coords = S_coords[S₅, :]
+    Plots.scatter(
+        BU_coords[:, 1],
+        BU_coords[:, 2],
+        label = "BUs",
+        markershape = :circle,
+        markercolor = :blue,
+    )
+    Plots.scatter!(
+        S₁_coords[:, 1],
+        S₁_coords[:, 2],
+        label = "Branches K₁",
+        markershape = :hexagon,
+        markercolor = is_used,
+        markersize = 6,
+        markerstrokecolor = :red,
+        markerstrokewidth = 2,
+    )
+    Plots.scatter!(
+        S₂_coords[:, 1],
+        S₂_coords[:, 2],
+        label = "Branches K₂",
+        markershape = :diamond,
+        markercolor = is_used,
+        markersize = 6,
+        markerstrokecolor = :red,
+        markerstrokewidth = 2,
+    )
+    Plots.scatter!(
+        S₃_coords[:, 1],
+        S₃_coords[:, 2],
+        label = "Branches K₃",
+        markershape = :heptagon,
+        markercolor = is_used,
+        markersize = 6,
+        markerstrokecolor = :red,
+        markerstrokewidth = 2,
+    )
+    Plots.scatter!(
+        S₄_coords[:, 1],
+        S₄_coords[:, 2],
+        label = "Branches K₄",
+        markershape = :pentagon,
+        markercolor = is_used,
+        markersize = 6,
+        markerstrokecolor = :red,
+        markerstrokewidth = 2,
+    )
+    Plots.scatter!(
+        S₅_coords[:, 1],
+        S₅_coords[:, 2],
+        label = "Branches K₅",
+        markershape = :star4,
+        markercolor = is_used,
+        markersize = 6,
+        markerstrokecolor = :red,
+        markerstrokewidth = 2,
+    )
+
+    for i in 1:B
+        for j in 1:S
+            if X[i, j] == 1
+                Plots.plot!(
+                    [BU_coords[i,1], S_coords[j,1]],
+                    [BU_coords[i,2], S_coords[j,2]],
+                    color = :black,
+                    label = nothing,
+                )
+            end
+        end
+    end
+    png(path)
+    @debug "Wrote plot"
+
 end
 
 function read_instance(path::String)
