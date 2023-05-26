ccd9a7adb3c5edf420d080e1101800933f1d0b3e
Eduardo Salazar <eduardosalaz@outlook.com>
2023-05-26 11:25:58 -0600

    p disp grasp

--- src/math_prog/pdisp.jl
+++ src/math_prog/pdisp.jl
@@ -50,7 +50,7 @@ function remove!(V, item)
     deleteat!(V, findall(x -> x == item, V))
 end
 
-function pdisp_simple(d, p, N)
+function pdisp_simple(d, p, N, α)
     maxdist  = 0
     bestpair = (0, 1)
     for i in 1:N
@@ -68,6 +68,7 @@ function pdisp_simple(d, p, N)
     while length(P) < p
         maxdist = 0
         vbest = 0
+        minimal = Inf
         for v in 1:N
             if v in P
                 continue
@@ -80,12 +81,36 @@ function pdisp_simple(d, p, N)
             end
             if mindist > maxdist
                 maxdist = mindist
-                vbest = v
+                minimal = mindist
             end
         end
-        if vbest != 0 && !(vbest in P)
-            
-            push!(P, vbest)
+        maxdist = 0
+        vbest = 0
+        rcl = []
+        for v in 1:N
+            if v in P
+                continue
+            end
+            mindist = Inf
+            for vprime in P
+                if d[v,vprime] < mindist
+                    mindist = d[v,vprime]
+                end
+            end
+            if mindist > maxdist
+                maxdist = mindist
+                if mindist <= round(Int, (minimal + minimal * α))
+                    push!(rcl, v)
+                end
+            end
+        end
+        selected = false
+        while !selected
+            v_candidate = rand(rcl)
+            if v_candidate != 0 && !(v_candidate in P)
+                push!(P, v_candidate)
+                selected = true
+            end
         end
     end
     collection = collect(P)
@@ -111,7 +136,7 @@ function node_type(i, Sk)
 end
 
 
-function pdisp_3(instance)
+function pdisp_heuristic(instance)
     B = instance.B
     S = instance.S
     D = instance.D
@@ -149,12 +174,13 @@ function pdisp_3(instance)
     p3 = Lk[3]
     p4 = Lk[4]
     p5 = Lk[5]
-
-    pdisp1 = pdisp_simple(d1, p1, N1)
-    pdisp2 = pdisp_simple(d2, p2, N2)
-    pdisp3 = pdisp_simple(d3, p3, N3)
-    pdisp4 = pdisp_simple(d4, p4, N4)
-    pdisp5 = pdisp_simple(d5, p5, N5)
+    N = S
+    α = 0.1
+    pdisp1 = pdisp_simple(d1, p1, N1, α)
+    pdisp2 = pdisp_simple(d2, p2, N2, α)
+    pdisp3 = pdisp_simple(d3, p3, N3, α)
+    pdisp4 = pdisp_simple(d4, p4, N4, α)
+    pdisp5 = pdisp_simple(d5, p5, N5, α)
 
     pdisp1_fixed = Sk[1][pdisp1]
     pdisp2_fixed = Sk[2][pdisp2]
@@ -169,6 +195,8 @@ function pdisp_3(instance)
             # Find the node v that maximizes the distance to its closest neighbor in P
             maxdist = 0
             vbest = 0
+            rcl = []
+            best_dist = 0
             for v in 1:N
                 if v in pdisp_ok
                     continue
@@ -180,11 +208,31 @@ function pdisp_3(instance)
                 dist = minimum([d[v,vprime] for vprime in pdisp_ok])
                 if dist > maxdist
                     maxdist = dist
-                    vbest = v
+                    best_dist = dist
+                    #vbest = v
                 end
             end
+            maxdist = 0
+            vbest = 0
+            rcl = []
+            for v in 1:N
+                if v in pdisp_ok
+                    continue
+                end
+                k = node_type(v, Sk)
+                if count[k] >= Uk[k]
+                    continue
+                end
+                dist = minimum([d[v,vprime] for vprime in pdisp_ok])
+                if dist <= round(Int, best_dist + best_dist * α)
+                    push!(rcl, v)
+                    #vbest = v
+                end
+            end
+            vbest = rand(rcl)
             # If no such node exists, stop the algorithm
             if vbest == 0
+                @error "P DISP ERROR"
                 break
             end
             # Add the node vbest to the set P and update the counts
@@ -200,173 +248,44 @@ function pdisp_3(instance)
     end
     return Y_bool
 end
-#=
-function pdisp(d, p, N, Sk, Lk, Uk)
-    # Find the pair of nodes with maximum distance
-    maxdist = 0
-    bestpair = (0, 1)
-    for i in 1:N
-        for j in i+1:N
-            if d[i,j] > maxdist
-                maxdist = d[i,j]
-                bestpair = (i,j)
-            end
-        end
-    end
-    P = Set([bestpair[1], bestpair[2]])
-    while length(P) < p
-        # Find the node v that maximizes the distance to its closest neighbor in P
-        maxdist = 0
-        vbest = 0
-        for v in 1:N
-            if v in P
-                continue
-            end
-            k = node_type(v, Sk)
-            if count[k] >= Uk[k]
-                continue
-            end
-            dist = minimum([d[v,vprime] for vprime in P])
-            if dist > maxdist
-                maxdist = dist
-                vbest = v
-            end
-        end
-        # If no such node exists, stop the algorithm
-        if vbest == 0
-            println("ni peyo")
-            break
-        end
-        # Add the node vbest to the set P and update the counts
-        k = node_type(vbest, Sk)
-        count[k] += 1
-        push!(P, vbest)
-    end
-    # Check if the solution satisfies the lower bounds
-    count = count_k(P, Sk)
-    for k = 1:length(Sk)
-        if count[k] < Lk[k]
-            println("puta mamada")
-            return Set([])
-        end
-    end
-    return P
-end
-=#
-
-function pdisp_2(instance)
-    B = instance.B
-    S = instance.S
-    D = instance.D
-    Sk = instance.Sk
-    Lk = instance.Lk
-    Uk = instance.Uk
-    p = instance.P
-    V = instance.V
-    μ = instance.μ
-    T = instance.T
-    R = instance.R
-    β = instance.β
-    k = 5
-    s_coords = instance.S_coords
-    metric = Distances.Euclidean()
-    d = trunc.(Int, Distances.pairwise(metric, s_coords, dims=1))
-    N = S
-    maxdist  = 0
-    bestpair = (0, 1)
-    for i in 1:N
-        for j in i+1:N
-            if d[i,j]>maxdist
-                maxdist = d[i,j]
-                bestpair = (i,j)
-            end
-        end
-    end
-
-    P = Set([])
-    push!(P, bestpair[1])
-    push!(P, bestpair[2])
-
-    while length(P) < p
-        maxdist = 0
-        vbest = 0
-        for v in 1:N
-            if v in P
-                continue
-            end
-            for vprime in P
-                if d[v,vprime]>maxdist
-                    maxdist = d[v,vprime]
-                    # indexar el arreglo de las Sk para saber que tipo es v
-                    # if Lk  >  countk
-                    vbest   = v
-                end
-            end
-        push!(P, vbest)
-        end
-    end
-    collection = collect(P)
-    Y_bool = zeros(Int, instance.S)
-    for idx in collection
-        Y_bool[idx] = 1
-    end
-    return Y_bool
-end
-
-function pdisp_heuristic(instance)
-    P = instance.P
-    s_coords = instance.S_coords
-    S = instance.S
-    metric = Distances.Euclidean()
-    s_distances = trunc.(Int, Distances.pairwise(metric, s_coords, dims=1))
-    idx_1, idx_2 = Tuple(argmax(s_distances)) # two furthest facs
-    S_sol = [idx_1, idx_2] # S is our solution of our p-disp problem
-    T = collect(1:S) # nodes not in our solution
-    # se puede usar la suma de xⱼ a S_sol
-    # o usar la facility mas cercana de xⱼ
-    for s in S_sol
-        remove!(T, s)
-    end
-
-    while length(S_sol) < P
-        distances = Tuple{Int64,Int64}[]
-        idx_max_dist = 0
-        for i in T
-            distance = 0
-            for j in S
-                distance += s_distances[i, j]
-            end
-            tupla = (distance, i)
-            push!(distances, tupla)
-        end
-        distances_max = [distance[1] for distance in distances]::Array{Int64}
-        indexes = [distance[2] for distance in distances]::Array{Int64}
-        idx_max = argmax(distances_max)::Int64
-        idx_max_dist = indexes[idx_max]
-        remove!(T, idx_max_dist)
-        push!(S_sol, idx_max_dist)
-    end
-    Y_bool = zeros(Int, instance.S)
-    for idx in S_sol
-        Y_bool[idx] = 1
-    end
-    return Y_bool
-end
 
 function main()
-    instance = read_instance("instances\\800_150_50\\inst_1_800_150_50.jld2")
+    instance = read_instance("instances\\625_78_32\\inst_1_625_78_32.jld2")
     println(@time pdisp_heuristic(instance))
     # println(@time pdisp_model(instance))
-    println(@time pdisp_2(instance))
-    println(@time pdisp_3(instance))
     Y_heur_1 = pdisp_heuristic(instance)
-    Y_heur_2 = pdisp_2(instance)
-    Y_heur_3 = pdisp_3(instance)
-    Y_model =  pdisp_model(instance)
     Y_heur1_bool = Bool.(Y_heur_1)
-    Y_heur2_bool = Bool.(Y_heur_2)
-    Y_heur3_bool = Bool.(Y_heur_3)
-    Y_model_bool = Bool.(Y_model)
+    Y = Y_heur1_bool
+    P = instance.P
+    if sum(Y_heur1_bool) != P
+            println("Violando número de centros asignados ", sum(Y), " $P")
+    end
+    K = 5
+    Sk = instance.Sk
+    Lk = instance.Lk
+    counts_k = []
+    Uk = instance.Uk
+    
+    for k_type in 1:K
+        indices_k_type = Sk[k_type]
+        count_k_type = 0
+        for indice in indices_k_type
+            if Y[indice] == 1
+                count_k_type += 1
+            end
+        end
+        push!(counts_k, count_k_type)
+    end
+
+    for k in 1:K
+        if counts_k[k] < Lk[k]
+                println("Violando Lk en $k")
+        end
+        if counts_k[k] > Uk[k]
+                println("Violando Uk en $k")
+        end
+    end
+
     S = instance.S
     plot_font = "Computer Modern"
     default(fontfamily=plot_font,framestyle=:box, grid=false, tickfontsize=7)
@@ -383,46 +302,7 @@ function main()
         label = nothing,
     )
 
-    Plots.pdf(p, "plot_heur1.pdf")
-
-    p = Plots.scatter(
-        S_coords[:,1],
-        S_coords[:,2],
-        markershape = :square,
-        markercolor = [(Y_heur2_bool[i] ? :red : :white ) for i in 1:S],
-        markersize = 6,
-        markerstrokecolor = :red,
-        markerstrokewidth = 2,
-        label = nothing,
-    )
-
-    Plots.pdf(p, "plot_heur2.pdf")
-
-    p = Plots.scatter(
-        S_coords[:,1],
-        S_coords[:,2],
-        markershape = :square,
-        markercolor = [(Y_heur3_bool[i] ? :red : :white ) for i in 1:S],
-        markersize = 6,
-        markerstrokecolor = :red,
-        markerstrokewidth = 2,
-        label = nothing,
-    )
-
-    Plots.pdf(p, "plot_heur3.pdf")
-
-    p2 = Plots.scatter(
-        S_coords[:,1],
-        S_coords[:,2],
-        markershape = :square,
-        markercolor = [(Y_model_bool[i] ? :red : :white) for i in 1:S],
-        markersize = 6,
-        markerstrokecolor = :red,
-        markerstrokewidth = 2,
-        label = nothing,
-    )
-
-    Plots.pdf(p2, "plot_model.pdf")
+    Plots.pdf(p, "plot_heur1_grasp2.pdf")
 end
 
 main()
